0bdad6dd743d7f474ae3c61cd221409217195d8f
nickmontoya2 <nickmontoya2@gmail.com>
2019-12-16 10:51:18 -0500

    Added CORS headers so requests can be made to front-end

--- src/main/java/com/skillstorm/controllers/CarController.java
+++ src/main/java/com/skillstorm/controllers/CarController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.skillstorm.service.CarService;
 
 @RestController
 @RequestMapping(value = "/cars") // Every URI that starts with /cars comes here
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class CarController {
 
 	@Autowired
